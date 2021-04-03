20281cf26630e8a91d4477932e18427446c4ea3d
Satwik <satwik929@gmail.com>
2021-04-03 15:22:22 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"satwik929@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
